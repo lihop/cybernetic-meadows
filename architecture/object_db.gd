38f37deb27e40abcb0d991b20cd0a8126a8e6cf6
Leroy Hopson <git@leroy.geek.nz>
2020-08-15 17:20:04 +0700

    Remove unused files

--- architecture/object_db.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-# The ObjectDB singleton contains a database of all objects it the game.
-# It can be consulted to dynamically generate menus in the user interface.
-
-
-func _ready():
-	# Register all objects based on directory.
-	
--- architecture/power/power.gd
+++
@@ -1,6 +0,0 @@
-extends Architecture
-class_name power
-
-
-# The ammount of power this item produces or consumes
-export(float) var power
